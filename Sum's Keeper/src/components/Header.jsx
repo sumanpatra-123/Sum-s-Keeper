4d5738290eabfedd88eafb4cbba04fec8452b271
sumanpatra-123 <64701665+sumanpatra-123@users.noreply.github.com>
2020-12-22 15:43:11 +0530

    Add files via upload

---
+++ Sum's Keeper/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ListAltOutlinedIcon from "@material-ui/icons/ListAltOutlined";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <ListAltOutlinedIcon /> Sum's Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
